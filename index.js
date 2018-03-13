c51d0e86d6f6caef27c8e9491dc2e0611241ffbf
Karim Ezzat <modosaki@gmail.com>
2018-03-13 20:29:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-var obj = { prop: propnumber}
-var recipes ={obj}
-
-
-var updateObjectWithKeyAndValue=(obj, 'prop2', 2)
-function newobj (prop,propnumber){
-  return Object.assign({},obj,{[prop]:propnumber})
-}
+var recipies ={}
